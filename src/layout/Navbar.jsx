82859ddd0cf231ebe4ed879c7f0be220268ce1c1
mdrefathossen <refathossen2001@gmail.com>
2023-12-30 17:01:00 +0600

    Create Navbar component with a simple logo

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react' 
+import '../assets/css/Navbar/navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <nav >
+            <a className='logo' href="">Logo</a>
+        </nav>
+    </div>
   )
 }
 
