01de6a13bcf9faac51c91c69957aab836b74c956
mdrefathossen <refathossen2001@gmail.com>
2024-01-06 19:04:31 +0600

    Add dynamic rendering of repository information

--- src/component/Repositorie.jsx
+++ src/component/Repositorie.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../context/UserContext'
+import '../assets/css/Repositorie/repositorie.css'
 import axios from 'axios'
 
 function Repositorie() {
@@ -12,8 +13,8 @@ function Repositorie() {
     useEffect(() => {
         axios.get(userData?.repos_url)
         .then(resp => {
-           
-            setRepoData(resp?.data)
+            const sortedRepositories = resp?.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+            setRepoData(sortedRepositories)
         }) 
         .catch(erro => {
             console.log(erro)
@@ -21,7 +22,26 @@ function Repositorie() {
     },[])
     console.log(repoData)
   return (
-    <div>Repositorie</div>
+    <div>
+        <div>
+        {/* Displaying a list of repositories with their information. */}
+            {
+                repoData && repoData.map((repositorie) => <>
+                <div className='repo-info'>
+                    <div className="repo_name">
+                        <h2>{repositorie?.name}</h2>
+                    </div>
+                    <div className="other-infof">
+                        <p>{repositorie?.language}</p>
+                        <p>{repositorie?.forks_count}</p>
+                        <p>{repositorie?.stargazers_count}</p>
+
+                    </div>
+                </div>
+                </>)
+            }
+        </div>
+    </div>
   )
 }
 
